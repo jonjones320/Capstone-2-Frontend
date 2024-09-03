ad57d4fe081c2c76c105cc657ac8963069bee128
JJ <jonjones320@gmail.com>
2024-09-02 23:26:37 -0700

    Added colors package for console feedback.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 /** Shared config for application; can be required many places. */
 
 require("dotenv").config();
-import "colors";
+require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
@@ -11,17 +11,16 @@ const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
-  return (process.env.NODE_ENV === "test")
+  return (process.env.NODE_ENV === "dev")
       ? process.env.DATABASE_TEST_URL || "postgresql:///ranner_test"
       : process.env.DATABASE_URL || "postgresql:///ranner";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
 //
-// WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
-const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "dev" ? 1 : 14;
 
-console.log("Jobly Config:".green);
+console.log("Ranner Config:".green);
 console.log("SECRET_KEY:".yellow, SECRET_KEY);
 console.log("PORT:".yellow, PORT.toString());
 console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
