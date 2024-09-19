2dbe8320be509cefd0346a5e736b7c47520292f2
JJ <jonjones320@gmail.com>
2024-09-18 23:03:48 -0700

    Added link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
   <div>
     <h1>Welcome to Ranner!</h1>
     <p>Start planning your next big adventure, one step at a time!</p>
+    <Link to="">Start</Link>
   </div>
 )
 }
