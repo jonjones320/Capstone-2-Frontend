0c3a3a8f2fb7933bcfd40bd9e52ec9e36f131cc0
JJ <jonjones320@gmail.com>
2024-09-23 04:46:40 -0700

    Removed unused navlink 'flights' and changed 'add new trip' to go to 'Origin.jsx'.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,9 +10,8 @@ function NavBar() {
     return (
       <nav className='NavBar-user'>
         <Link to="/">Home</Link>
-        <Link to="/flights">Flights</Link>
         <Link to="/trips">Trips</Link>
-        <Link to="/trip">Add New Trip</Link>
+        <Link to="/origin">Add New Trip</Link>
         <Link to="/profile">Profile</Link>
         <Link to="/logout">Logout</Link>
       </nav>
