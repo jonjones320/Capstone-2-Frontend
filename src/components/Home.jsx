78f54196d201b0a59b847ceb7bda703eab2669b9
JJ <jonjones320@gmail.com>
2024-09-24 02:39:36 -0700

    Added general styling using react-bootstrap.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Container, Row, Col, Button, Card } from 'react-bootstrap';
 
 function Home() {
   return (
-  <div>
-    <h1>Welcome to Ranner!</h1>
-    <p>Start planning your next big adventure, one step at a time!</p>
-    <Link to="/origin">Start</Link>
-  </div>
-)
+    <Container className="mt-5">
+      <Row className="justify-content-center">
+        <Col md={8} className="text-center">
+          <Card className="shadow-sm">
+            <Card.Body>
+              <h1 className="display-4 mb-4">Welcome to Ranner!</h1>
+              <p className="lead mb-4">Start planning your next big adventure, one step at a time!</p>
+              <Link to="/origin">
+                <Button variant="primary" size="lg">Start Your Journey</Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Home;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,36 @@
 import React, { useContext } from 'react';
-import AuthContext from '../context/AuthContext';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-
+import AuthContext from '../context/AuthContext';
 
 function NavBar() {
   const { currentUser } = useContext(AuthContext);
   
-  if (currentUser) {
-    return (
-      <nav className='NavBar-user'>
-        <Link to="/">Home</Link>
-        <Link to="/trips">Trips</Link>
-        <Link to="/origin">Add New Trip</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/logout">Logout</Link>
-      </nav>
-    );
-  } else {
-    return (
-      <nav className='NavBar-guest'>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Signup</Link>
-      </nav>
-    );
-  }
+  return (
+    <Navbar bg="light" expand="lg" className="mb-3">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Ranner</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            {currentUser ? (
+              <>
+                <Nav.Link as={Link} to="/trips">My Trips</Nav.Link>
+                <Nav.Link as={Link} to="/origin">New Trip</Nav.Link>
+                <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+                <Nav.Link as={Link} to="/logout">Logout</Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavBar;
