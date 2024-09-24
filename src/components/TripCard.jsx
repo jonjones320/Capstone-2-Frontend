a4c0b4db5b3e8fd2bfca64b98d9a00497eacf76f
JJ <jonjones320@gmail.com>
2024-09-24 03:17:18 -0700

    Added date formatting so trip dates will display YYYY-MM-DD.

--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -5,16 +5,16 @@ import { Link } from 'react-router-dom';
 function TripCard({ id, name, origin, destination, startDate, endDate }) {
   return (
     <Card key={id}>
-      <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Text>
-          <strong>Origin:</strong> {origin}<br />
-          <strong>Destination:</strong> {destination}<br />
-          <strong>Dates:</strong> {startDate} - {endDate}
-        </Card.Text>
-        <Link to={`/trip/${id}`} className="btn btn-primary btn-sm">View Details</Link>
-      </Card.Body>
-    </Card>
+    <Card.Body>
+      <Card.Title>{name}</Card.Title>
+      <Card.Text>
+        <strong>Origin:</strong> {origin}<br />
+        <strong>Destination:</strong> {destination}<br />
+        <strong>Dates:</strong> {new Date(startDate).toLocaleDateString()} - {new Date(endDate).toLocaleDateString()}
+      </Card.Text>
+      <Link to={`/trip/${id}`} className="btn btn-primary btn-sm">View Details</Link>
+    </Card.Body>
+  </Card>
   );
 }
 
