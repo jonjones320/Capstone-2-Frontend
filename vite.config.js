ef97e3be28dbb777dff65f7d0db8cb1088028d6c
JJ <jonjones320@gmail.com>
2024-10-11 05:57:33 -0700

    Defined the location of the environment setting so Vite knows where to find 'production' or 'development'.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env
+    'process.env': process.env,
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
   },
   server: {
     host: '0.0.0.0',
