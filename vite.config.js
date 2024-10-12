0d87beb8f9ef763a47e6191094f72fb6e726264d
JJ <jonjones320@gmail.com>
2024-10-11 21:20:06 -0700

    Removed previous declarations within the 'define' object. It was unneeded for Render.

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env,
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    'process.env': process.env
   },
   server: {
     host: '0.0.0.0',
