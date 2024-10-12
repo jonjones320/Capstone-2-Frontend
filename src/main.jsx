444f19601785ae15d6d79d8a8aadca6ea88baf96
JJ <jonjones320@gmail.com>
2024-10-11 22:28:22 -0700

    Removed debugging logs

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-console.log('Starting application...');
-
 const root = document.getElementById('root');
-console.log('Root element found:', root);
 
 createRoot(root).render(
   <StrictMode>
     <App />
   </StrictMode>
 )
-
-console.log('Application rendered');
