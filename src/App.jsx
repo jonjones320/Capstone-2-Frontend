d0412030269d87215a15758a26b5ab15be553396
JJ <jonjones320@gmail.com>
2024-10-20 11:40:08 -0700

    Import custom.css style class. Wrapped the app components in JSX to apply universal CSS. Plus 'main-content' and 'footer'.

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,25 @@ import Routes from './Routes.jsx'
 import NavBar from './components/NavBar'
 import { Container } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/custom.css';
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <NavBar />
-        <Container>
-          <Routes />
-        </Container>
+        <div className="app-wrapper">
+          <NavBar />
+          <main className="main-content">
+            <Container>
+              <Routes />
+            </Container>
+          </main>
+          <footer className="app-footer">
+            <Container>
+              <p>&copy; 2024 Ranner. All rights reserved.</p>
+            </Container>
+          </footer>
+        </div>
       </BrowserRouter>
     </AuthProvider>
   )
