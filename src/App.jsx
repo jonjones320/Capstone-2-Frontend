be8b5fefec31ac445af8ff30e963f652af2186a0
JJ <jonjones320@gmail.com>
2024-10-25 00:50:22 -0700

    Wrapped the app in the ErrorBoundary component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,38 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext.jsx'
-import Routes from './Routes.jsx'
-import NavBar from './components/NavBar'
-import { Container } from 'react-bootstrap'
+// App.jsx
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext.jsx';
+import Routes from './Routes.jsx';
+import NavBar from './components/NavBar';
+import ErrorBoundary from './components/ErrorBoundary';
+import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/custom.css';
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <div className="app-wrapper">
-          <NavBar />
-          <main className="main-content">
-            <Container>
-              <Routes />
-            </Container>
-          </main>
-          <footer className="app-footer">
-            <Container>
-              <p>&copy; 2024 Ranner. All rights reserved.</p>
-            </Container>
-          </footer>
-        </div>
-      </BrowserRouter>
-    </AuthProvider>
-  )
+    <ErrorBoundary>
+      <AuthProvider>
+        <BrowserRouter>
+          <div className="app-wrapper">
+            <NavBar />
+            <main className="main-content">
+              <Container>
+                <ErrorBoundary>
+                  <Routes />
+                </ErrorBoundary>
+              </Container>
+            </main>
+            <footer className="app-footer">
+              <Container>
+                <p>&copy; 2024 Ranner. All rights reserved.</p>
+              </Container>
+            </footer>
+          </div>
+        </BrowserRouter>
+      </AuthProvider>
+    </ErrorBoundary>
+  );
 }
 
-export default App
+export default App;
