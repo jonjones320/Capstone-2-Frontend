1ee27cb3ae6db538aebf3b92cc7d0959b7e2fd0f
JJ <jonjones320@gmail.com>
2024-10-26 03:01:51 -0700

    Fixed api.js import.

--- src/__tests__/components/Destination.test.jsx
+++ src/__tests__/components/Destination.test.jsx
@@ -1,7 +1,7 @@
 import { screen, fireEvent, waitFor } from '@testing-library/react';
 import { renderWithContext } from '../testUtils';
 import Destination from '../../components/Destination';
-import RannerApi from '../../api';
+import RannerApi from '../../../api';
 
 const mockState = {
   origin: 'SFO'
