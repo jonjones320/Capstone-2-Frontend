20b8e9673f381481882b60d6475b7dfc30f9973f
JJ <jonjones320@gmail.com>
2024-10-26 04:50:48 -0700

    Import updated to new 'testUtils.jsx' location in 'utils' directory.

--- src/__tests__/components/Destination.test.jsx
+++ src/__tests__/components/Destination.test.jsx
@@ -1,6 +1,5 @@
 import { screen, fireEvent } from '@testing-library/react';
-import { renderWithContext } from '../helpers/testUtils';
-import { findAlertMessage } from '../helpers/testUtils';
+import { renderWithContext, findAlertMessage } from '../utils/testUtils';
 import Destination from '../../components/Destination';
 import RannerApi from '../../api';
 
