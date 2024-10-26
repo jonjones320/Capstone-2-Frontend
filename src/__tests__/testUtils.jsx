7e0147cc218a2987273e79ea76b7b66689f83107
JJ <jonjones320@gmail.com>
2024-10-25 22:38:52 -0700

    React specific portion of test setup separated from Common JS file 'test.cjs'.

---
+++ src/__tests__/testUtils.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from '../context/AuthContext';
+
+export function renderWithContext(ui, { route = '/', user = null } = {}) {
+  window.history.pushState({}, 'Test page', route);
+
+  return render(
+    <AuthProvider initialUser={user}>
+      <BrowserRouter>{ui}</BrowserRouter>
+    </AuthProvider>
+  );
+}
