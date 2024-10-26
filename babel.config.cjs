50f3a5ca889f9c2db7d67aedc2bb40c20e2903cb
JJ <jonjones320@gmail.com>
2024-10-25 22:17:54 -0700

    Clarified that the babel config file needs to be run as Common JS.

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
-    presets: [
-      ['@babel/preset-env', { targets: { node: 'current' } }],
-      ['@babel/preset-react', { runtime: 'automatic' }]
-    ],
-  };
+  presets: [
+    ['@babel/preset-env', { 
+      targets: { node: 'current' },
+      modules: 'commonjs'  // Vite defaults to expecting ES JS files. This tells it to use Common Js.
+    }],
+    ['@babel/preset-react', { runtime: 'automatic' }]
+  ],
+};
