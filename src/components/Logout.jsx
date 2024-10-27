ef2d77b5bd48f9295415f4eca0014017d341db72
JJ <jonjones320@gmail.com>
2024-10-27 02:22:17 -0700

    Imported useState.

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col, Button, Spinner } from 'react-bootstrap';
 import AuthContext from '../context/AuthContext';
