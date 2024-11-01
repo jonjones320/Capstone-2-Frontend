5f1cbed63d8b9639c8996c1ec4335b9fcd3a9fa0
JJ <jonjones320@gmail.com>
2024-11-01 08:03:29 -0700

    Added state import.

--- src/components/FlightCard.jsx
+++ src/components/FlightCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RannerApi from '../../api';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Badge, Button, Alert } from 'react-bootstrap';
