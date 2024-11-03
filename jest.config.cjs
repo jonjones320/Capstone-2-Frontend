e5d40452a1ed7046faa6de323eea0a430c9603d8
JJ <jonjones320@gmail.com>
2024-11-03 06:39:43 -0800

    Removed Bootstrap mock.

--- jest.config.cjs
+++ jest.config.cjs
@@ -8,8 +8,6 @@ module.exports = {
     '^react-router-dom$': '/home/jonjones320/Springboard/Capstone/Trip_Planner/Ranner-Frontend/src/__tests__/__mocks__/react-router-dom.js',
     // Handle CSS imports
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    // Mock Bootstrap CSS specifically
-    'bootstrap/dist/css/bootstrap.min.css': 'identity-obj-proxy',
     // Handle image imports
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '/home/jonjones320/Springboard/Capstone/Trip_Planner/Ranner-Frontend/src/__tests__/__mocks__/fileMock.js',
