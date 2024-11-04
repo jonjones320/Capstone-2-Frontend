db9c3e2e99c17a80852690fb0abb493a57c7339e
JJ <jonjones320@gmail.com>
2024-11-03 23:52:48 -0800

    Fixed AuthContext import by adding destructuring for named import.

--- src/__tests__/utils/testUtils.jsx
+++ src/__tests__/utils/testUtils.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import AuthContext from '../../context/AuthContext';
+import { AuthContext } from '../../context/AuthContext';
 
 // Custom render with providers.
 const defaultAuthContext = {
