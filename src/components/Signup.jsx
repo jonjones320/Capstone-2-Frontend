9b3438536b89ca0f0e13397836df90818e74c906
JJ <jonjones320@gmail.com>
2024-11-11 15:12:07 -0800

    Added import for useErrorHandler.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Form, Button, Container, Row, Col, Spinner, Alert } from 'react-bootstrap';
 import { AuthContext } from '../context/AuthContext';
-import { ErrorHandler } from '../utils/errorHandler';
+import { ErrorHandler, useErrorHandler  } from '../utils/errorHandler';
 import ErrorAlert from './ErrorAlert';
 import RannerApi from '../../api';
 
