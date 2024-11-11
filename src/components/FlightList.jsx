318f31c9933fab5f466c8ad495f30c8dd861107d
JJ <jonjones320@gmail.com>
2024-11-11 11:21:34 -0800

    Imported ErrorHandler to use handleApiError when fetching flights goes wrong.

--- src/components/FlightList.jsx
+++ src/components/FlightList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { Container, Spinner, Button, Card, Alert } from 'react-bootstrap';
+import { ErrorHandler } from '../utils/errorHandler';
 import RannerApi from '../../api';
 import FlightCard from './FlightCard';
 
